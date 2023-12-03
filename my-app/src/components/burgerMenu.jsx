05eb5c3f6b154010e74659b802bfd579c3f8e722
Alexgbp <bboy91@bk.ru>
2023-12-03 11:58:00 +0300

    sc

--- my-app/src/components/burgerMenu.jsx
+++ my-app/src/components/burgerMenu.jsx
@@ -1,59 +1,6 @@
 import React from 'react';
 const { useState } = React;
-import styled from "styled-components";
-
-
-const BurgerNav = styled.div`
-cursor: pointer;
-width: 20px;
-height: 36px;
-padding: 13px 0;
-display: -webkit-box;
-display: -ms-flexbox;
-display: flex;
--webkit-box-orient: vertical;
--webkit-box-direction: normal;
--ms-flex-direction: column;
-flex-direction: column;
--webkit-box-pack: justify;
--ms-flex-pack: justify;
-justify-content: space-between;
-`
-
-const BurgerLine = styled.span`
-  display: inline-block;
-  width: 100%;
-  height: 1px;
-  background-color: #d3d3d3;
-`;
-
-const MenuList = styled.ul`
-padding: 18px 0 10px 0;
-`
-
-const MenuItem = styled.li`
-  padding: 5px 0;
-  margin-bottom: 16px;
-`;
-
-const MenuLink = styled.a`
-  color: #ffffff;
-  font-weight: 400;
-  font-size: 16px;
-  line-height: 24px;
-  cursor: pointer;
-  &: hover{
-    color: #ad61ff;
-    transition: .3s all;
-    text-decoration: underline;
-  }
-`;
-
-const NavMenu = styled.div`
-  display: block;
-  visibility: ${(props) => props.$visible ? "visible" : "hidden" };
-  transition: 0.3s;
-`;
+import * as S from "./css/navMenu"
 
 function BurgerMenu() {
   const [visible, setVisible] = useState(false);
@@ -61,30 +8,30 @@ function BurgerMenu() {
 
   return (
     <nav>
-      <BurgerNav onClick={toglleState}>
-        <BurgerLine />
-        <BurgerLine />
-        <BurgerLine />
-      </BurgerNav>
-      <NavMenu $visible={visible}>
-        <MenuList>
-          <MenuItem>
-            <MenuLink href="#">
+      <S.BurgerNav onClick={toglleState}>
+        <S.BurgerLine />
+        <S.BurgerLine />
+        <S.BurgerLine />
+      </S.BurgerNav>
+      <S.NavMenu $visible={visible}>
+        <S.MenuList>
+          <S.MenuItem>
+            <S.MenuLink href="#">
               Главное
-            </MenuLink>
-          </MenuItem>
-          <MenuItem>
-            <MenuLink href="#">
+            </S.MenuLink>
+          </S.MenuItem>
+          <S.MenuItem>
+            <S.MenuLink href="#">
               Мой плейлист
-            </MenuLink>
-          </MenuItem>
-          <MenuItem>
-            <MenuLink href="../signin.html">
+            </S.MenuLink>
+          </S.MenuItem>
+          <S.MenuItem>
+            <S.MenuLink href="../signin.html">
               Войти
-            </MenuLink>
-          </MenuItem>
-        </MenuList>
-      </NavMenu>
+            </S.MenuLink>
+          </S.MenuItem>
+        </S.MenuList>
+      </S.NavMenu>
     </nav>
   );
 }
--- my-app/src/components/navMenu.jsx
+++ my-app/src/components/navMenu.jsx
@@ -1,35 +1,15 @@
 import React from 'react';
-// import './css/navMenu.css';
 import BurgerMenu from './burgerMenu';
-import styled from 'styled-components';
-
-const MenuNav = styled.nav`
-width: 244px;
-background-color: #181818;
-padding: 20px 0 20px 36px;
-`
-const LogoNav = styled.div`
-  width: 113.33px;
-  height: 43px;
-  padding: 13px 0 13px 0;
-  background-color: transparent;
-  margin-bottom: 20px;
-`;
-
-const ImageNav = styled.img`
-  width: 113.33px;
-  height: 17px;
-  color: #181818;
-`;
+import * as S from "./css/navMenu"
 
 function NavMenu() {
   return (
-    <MenuNav>
-      <LogoNav>
-      <ImageNav src="img/logo.png" alt="logo" />
-      </LogoNav>
+    <S.MenuNav>
+      <S.LogoNav>
+      <S.ImageNav src="img/logo.png" alt="logo" />
+      </S.LogoNav>
       <BurgerMenu />
-    </MenuNav>
+    </S.MenuNav>
   );
 }
 export default NavMenu;
