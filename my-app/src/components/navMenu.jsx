a1ee765522c361a0504142a19b393e48b0c49728
Alexgbp <bboy91@bk.ru>
2023-12-03 08:17:21 +0300

    sc

--- my-app/src/components/navMenu.jsx
+++ my-app/src/components/navMenu.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import './css/navMenu.css';
 import BurgerMenu from './burgerMenu';
+import styled from 'styled-components';
+
+const MenuNav = styled.nav`
+width: 244px;
+background-color: #181818;
+padding: 20px 0 20px 36px;
+`
+const LogoNav = styled.div`
+  width: 113.33px;
+  height: 43px;
+  padding: 13px 0 13px 0;
+  background-color: transparent;
+  margin-bottom: 20px;
+`;
+
+const ImageNav = styled.img`
+  width: 113.33px;
+  height: 17px;
+  color: #181818;
+`;
 
 function NavMenu() {
   return (
-    <nav className="main__nav nav">
-      <div className="nav__logo logo">
-        <img className="logo__image" src="img/logo.png" alt="logo" />
-      </div>
+    <MenuNav>
+      <LogoNav>
+      <ImageNav src="img/logo.png" alt="logo" />
+      </LogoNav>
       <BurgerMenu />
-    </nav>
+    </MenuNav>
   );
 }
 export default NavMenu;
