68d4a4dc5c40ff2a9e973648fa5d471f2937601d
Alexgbp <bboy91@bk.ru>
2023-12-05 21:54:56 +0300

    sc

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css';
 import React, { useEffect, useState } from 'react';
 import NavMenu from './components/navMenu';
 import SearchBlock from './components/searchBlock';
@@ -7,7 +6,12 @@ import AudioPlayer from './components/audioplayer/AudioPlayer.jsx';
 import Filter from './components/filter';
 import TrackList from './components/trackList';
 import { GlobalStyle } from './Global.styled.js';
-
+import * as S1 from './components/css/main-center-block.js';
+import * as S2 from './components/css/center-block-h2.js';
+import * as S3 from './components/css/footer.js';
+import * as S4 from './components/css/wrapper.js';
+import * as S5 from './components/css/container.js';
+import * as S6 from './components/css/main.js';
 
 function App() {
   const [loader, setLoader] = useState(false);
@@ -23,31 +27,23 @@ function App() {
 
   return (
     <>
-    <GlobalStyle />
-       <div className="wrapper">
-      <div className="container">
-        <div>
-          <main className="main">
+      <GlobalStyle />
+      <S4.Wrapper>
+        <S5.Container>
+          <S6.Main>
             <NavMenu />
-            <div className="main__centerblock centerblock">
+            <S1.MainCenterBlock>
               <SearchBlock />
-              <h2 className="centerblock__h2">Треки</h2>
+              <S2.CenterBlockH2>Треки</S2.CenterBlockH2>
               <Filter />
-              <TrackList 
-              loader={loader}
-              />
-            </div>
-            <SideBar 
-            loader={loader}
-            />
-          </main>
-          <AudioPlayer
-          loader={loader}
-          />
-          <footer className="footer" />
-        </div>
-      </div>
-    </div>
+              <TrackList loader={loader} />
+            </S1.MainCenterBlock>
+            <SideBar loader={loader} />
+          </S6.Main>
+          <AudioPlayer loader={loader} />
+          <S3.FooterBlock />
+        </S5.Container>
+      </S4.Wrapper>
     </>
   );
 }
