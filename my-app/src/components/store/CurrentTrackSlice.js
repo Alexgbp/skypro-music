ed68c590a6a3a90a9d87eb335cd313b38a5d3e6d
Alexgbp <bboy91@bk.ru>
2024-02-07 20:24:47 +0300

    redux

---
+++ my-app/src/components/store/CurrentTrackSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const currenTrackSlice = createSlice({
+    name: "currenTrackSlice",
+    initialState: {
+    
+    },
+    reducers:{
+    }
+})
+
+export default currenTrackSlice.reducer;
--- my-app/src/components/store/store.js
+++ my-app/src/components/store/store.js
@@ -3,5 +3,5 @@ import { configureStore } from "@reduxjs/toolkit";
 export default configureStore({
     reducer: {
         
-    }
+    },
 })
