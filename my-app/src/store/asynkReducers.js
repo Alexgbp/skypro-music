4a47b0faf3cc737ace41b58199a88fe9abd0e675
Alexgbp <bboy91@bk.ru>
2024-02-24 20:43:34 +0300

    add

--- my-app/src/store/asynkReducers.js
+++ my-app/src/store/asynkReducers.js
@@ -1,26 +1,10 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-// import { tokenUser } from "../../api/api";
-const accessToken =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjkwOTcxMjcxLCJpYXQiOjE2OTA5NjAxMzEsImp0aSI6ImE4YzQ5NDNmOWNmNTRlZjI5NmFmNTMyOWUwODM4YWQ5IiwidXNlcl9pZCI6NzkyfQ.5n8YHTjsgAnYnc4gioyV1wPnxM2D16PS6c9kNhC-JoE";
 
-export  const AddLike = createAsyncThunk(
-    "track/like",
-     async function(id, {rejectWithValue}){
-       try {
-        const response =  await fetch(`https://skypro-music-api.skyeng.tech/catalog/track/${id}/favorite/`, {
-            method: "POST",
-            headers: { 
-                Authorization: `Bearer ${accessToken}`  
-            },
-        })
-        if(!response.ok){
-            throw new Error("Ошибка")
-        }
-        // dispatch(tokenUser(email, passowrd))
-        const data = response.json()
-        return data;
-       } catch (error) {
-        return rejectWithValue(error.message)
-       }
-     }
+export const AddLike = createAsyncThunk(
+  "track/addLike",
+  async function(id){
+      const response = await fetch(`https://skypro-music-api.skyeng.tech/catalog/track/${id}/favorite/`)
+      const data = response.json()
+      return data
+  }
 )
