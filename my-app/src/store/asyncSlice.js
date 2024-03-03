a148d26bf72dcddaaf6906f47807f24155be0f3d
Alexgbp <bboy91@bk.ru>
2024-03-03 15:32:45 +0300

    async

--- my-app/src/store/asyncSlice.js
+++ my-app/src/store/asyncSlice.js
@@ -4,18 +4,29 @@ import { AddLike, DeleteLike } from "./apiForRedux";
 export const asyncSlice = createSlice({
   name: "track/Like",
   initialState: {
-    favoriteTracks: []
+    favoriteTracks: [],
+    error: null,
+    status: null
   },
   reducers: {
-
+    addLike(state, action){
+      state.favoriteTracks = action.payload
+    },
+    deleteLike(state, action){
+      state.favoriteTracks = state.favoriteTracks.map((element) => element.id !== action.payload)
+    }
   },
   extraReducers: (builder) => {
     builder
     .addCase(AddLike.rejected, (state, action) => {
       state.error = action.payload;
+      state.status = "rejected"
     })
     .addCase(DeleteLike.rejected, (state, action) => {
       state.error = action.payload;
+      state.status = "rejected"
     })
   },
 })
+
+export const {addLike, deleteLike} = asyncSlice.actions;
